6b01ae314c03c0327399609e2f50c5f4ae097d34
paulesthor <paul.esthor@etu.univ-poitiers.fr>
2026-02-08 12:18:51 +0100

    fix: refactor viewport metadata for next.js 14+

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,13 @@ const epilogue = Epilogue({
 export const metadata: Metadata = {
   title: "Fap Map",
   description: "Social Map for Gen Z",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
